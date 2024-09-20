80fe17cbedb7610df79e21410711fc2a75782430
Demian26 <158134773+Demian26@users.noreply.github.com>
2024-09-20 23:04:51 +0300

    Final Knight, Enemy AI, Cliff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -98,7 +98,16 @@ public class PlayerController : MonoBehaviour
     public bool CanMove { get
         {
             return animator.GetBool(AnimationStrings.canMove);
-        }}
+        }
+    }
+
+    public bool IsAlive { 
+        get
+        {
+            return animator.GetBool(AnimationStrings.isAlive);
+        }
+       
+    }
 
     Rigidbody2D rb;
     Animator animator;
@@ -122,9 +131,17 @@ public class PlayerController : MonoBehaviour
     {
         moveInput = context.ReadValue<Vector2>();
 
-        IsMoving = moveInput != Vector2.zero;
+        if(IsAlive)
+        {
+            IsMoving = moveInput != Vector2.zero;
 
-        SetFacingDirection(moveInput);
+            SetFacingDirection(moveInput);
+        } 
+        else
+        {
+            IsMoving = false;
+        }
+       
     }
 
     private void SetFacingDirection(Vector2 moveInput)
