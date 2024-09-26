f92ccc2cfb2b17597b35958bf3ee10eb31007e18
Demian26 <158134773+Demian26@users.noreply.github.com>
2024-09-26 22:42:52 +0300

    Flying Enemy

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -7,6 +7,7 @@ using UnityEngine.Events;
 public class Damageable : MonoBehaviour
 {
     public UnityEvent <int, Vector2> damageableHit;
+    public UnityEvent damageableDeath;
 
     Animator animator;
 
@@ -75,6 +76,11 @@ public class Damageable : MonoBehaviour
             _isAlive = value;
             animator.SetBool(AnimationStrings.isAlive, value);
             Debug.Log("IsAlive set " + value );
+
+            if(value ==false)
+            {
+                damageableDeath.Invoke();
+            }
         }
     }
      
